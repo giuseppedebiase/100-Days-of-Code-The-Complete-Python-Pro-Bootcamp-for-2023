e6670cae59410386901446d799a8ae55c1dd8575
giuseppe.debiase <133350197+giuseppedebiase@users.noreply.github.com>
2023-05-29 21:08:10 +0200

    Update main.py

--- Day 2 - Tip Calculator/main.py
+++ Day 2 - Tip Calculator/main.py
@@ -1,5 +1,6 @@
 bill = float(input("What was the total bill? "))
 tipperc = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+#Calculates the total amount of bill pluss tip
 tot = (bill+bill*tipperc/100)
 numpeople = int(input("How many people to split the bill? "))
 totround = round(tot/numpeople, 2)
