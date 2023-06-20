810df0e28a0be5f697bc5b3bce3f73ec7ddd37b7
giuseppe.debiase <133350197+giuseppedebiase@users.noreply.github.com>
2023-06-20 20:19:24 +0200

    Update main.py

--- Day 8 - Caesar Cipher/main.py
+++ Day 8 - Caesar Cipher/main.py
@@ -1,8 +1,9 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 import art
 print(art.logo)
 
+#this function encodes and decodes text through the caesar cipher
 def caesar(direction, text, shift):
   if shift > 26:
     shift = shift % 26
@@ -21,8 +22,10 @@ def caesar(direction, text, shift):
 
 answer = "yes"
 while answer == "yes":
+  #the direction asks if the user want to decode or encode a message
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
   text = input("Type your message:\n").lower()
+  #the shift is the key of the cipher
   shift = int(input("Type the shift number:\n"))
   caesar(direction, text, shift)
   answer = input("Do you want to try again? Type 'yes' or 'no'.\n")
